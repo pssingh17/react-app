c827a7c61b1bb37c5e964d9964faac6ce51f544a
PS <pssingh1789@gmail.com>
2021-05-15 19:10:42 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// function openNav() {
+//   // document.getElementById("nav-list").style.display = "block";
+//   document.getElementById("nav-list").style.left = "0%";
+// }
+  
+// /* Set the width of the side navigation to 0 */
+// function closeNav() {
+//   // document.getElementById("nav-list").style.display = "none";
+//   document.getElementById("nav-list").style.left = "-100%";
+// }
+// navitemclick = document.getElementsByClassName("nav-items");
+
+// navitemclick[0].addEventListener('click',()=>{
+    
+//     document.getElementById("nav-list").style.left = "-100%";
+// })
+// navitemclick[1].addEventListener('click',()=>{
+//    document.getElementById("nav-list").style.left = "-100%";
+    
+// })
+// navitemclick[2].addEventListener('click',()=>{
+    
+//     document.getElementById("nav-list").style.left = "-100%";
+// })
+// navitemclick[3].addEventListener('click',()=>{
+    
+//     document.getElementById("nav-list").style.left = "-100%";
+// })
+
+
+
+
