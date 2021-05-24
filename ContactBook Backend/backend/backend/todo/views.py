52d0d94b848272fd665eb3c7e701c2a543536922
Prabhsimran Singh <71607763+pssingh17@users.noreply.github.com>
2021-05-24 20:29:20 +0530

    Add files via upload

--- ContactBook Backend/backend/backend/todo/views.py
+++ ContactBook Backend/backend/backend/todo/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ContactBookSerializer
 from .models import ContactBook
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class ContactBookView(viewsets.ModelViewSet):
     serializer_class = ContactBookSerializer
     queryset = ContactBook.objects.all()
+    authentication_classes = [SessionAuthentication]
+    # permission_classes = [IsAuthenticated]  
