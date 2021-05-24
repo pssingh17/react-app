aabef36d92e15d34517bb748724b598af53c79ba
Prabhsimran Singh <71607763+pssingh17@users.noreply.github.com>
2021-05-24 20:28:10 +0530

    Add files via upload

--- ContactBook Backend/backend/backend/urls.py
+++ ContactBook Backend/backend/backend/urls.py
@@ -9,4 +9,5 @@ router.register(r'contactbook', views.ContactBookView, 'contactbook')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('auth/', include('rest_framework.urls' ,namespace='rest_framework')),
 ]
